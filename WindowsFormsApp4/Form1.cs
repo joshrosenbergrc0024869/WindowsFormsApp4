41f15a1608f3aa2ce2e10c5dc6740fd4c6a02ef1
Rosenberg <RC0024869@student.riversidecollege.ac.uk>
2018-07-02 10:29:17 +0100

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            double time;
+            double charge;
+        }
+
+        list
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
